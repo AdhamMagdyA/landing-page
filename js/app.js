f72458cd90a3ecfc2fbb864ee066ae163299c6ed
Adham Magdy <adhammagdy.dev@gmail.com>
2022-05-19 14:14:42 +0200

    created required task

--- js/app.js
+++ js/app.js
@@ -23,6 +23,14 @@
  * 
 */
 
+const startingTime = performance.now();
+
+
+
+const navBarList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+
+
 
 /**
  * End Global Variables
@@ -31,6 +39,15 @@
 */
 
 
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
 /**
  * End Helper Functions
@@ -39,12 +56,38 @@
 */
 
 // build the nav
+for(let section of sections){
+    const listItem = document.createElement('li');
+    listItem.innerHTML = section.id;
+    listItem.classList.add('menu__link');
+    listItem.id = section.id+'__link';
 
+    navBarList.appendChild(listItem);
+}
 
 // Add class 'active' to section when near top of viewport
-
+function setActiveSections(section){
+    if(isInViewport(section.querySelector('h2'))){
+        document.getElementById(section.id+'__link').classList.add('active');
+        section.classList.add('active');
+    }
+    if(!isInViewport(section.querySelector('h2'))){
+        document.getElementById(section.id+'__link').classList.remove('active');
+        section.classList.remove('active');
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
+document.querySelectorAll('.menu__link').forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = event.target.id.split('__')[0];
+        window.scrollTo({
+            top: document.getElementById(targetId).offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
 
 
 /**
@@ -56,7 +99,14 @@
 // Build menu 
 
 // Scroll to section on link click
+function scrollToSection() {
+    for(let section of sections){
+        setActiveSections(section);
+    }
+}
 
 // Set sections as active
+document.addEventListener('scroll', scrollToSection);
 
-
+const endingTime = performance.now();
+console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
