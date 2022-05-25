2fe6a2e96c092a1abbf54d6ef23a51c5920bec46
Adham Magdy <adhammagdy.dev@gmail.com>
2022-05-25 10:03:24 +0200

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,41 +1,12 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
+// global variables to be used throughout the program
 const startingTime = performance.now();
 const navBarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
 
+////////////////////// Functions definitions //////////////////////
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
+// check if an element is appearing on the screen
 isElementAppearing = (element) =>{
     const elementArea = element.getBoundingClientRect();
     // return true if the element top is under the top of the viewport
@@ -44,24 +15,21 @@ isElementAppearing = (element) =>{
 
 }
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// loop over the sections to create the menue items
+buildNavigationBar = (sections)=>{
+    for(let section of sections){
+        const listItem = document.createElement('li');
+        // prepare the list item
+        listItem.innerHTML = section.id;
+        listItem.classList.add('menu__link');
+        listItem.id = section.id+'__link';
+        // add the list item to the navbar
+        navBarList.appendChild(listItem);
+    }
+} 
 
-// build the nav
-for(let section of sections){
-    const listItem = document.createElement('li');
-    listItem.innerHTML = section.id;
-    listItem.classList.add('menu__link');
-    listItem.id = section.id+'__link';
-
-    navBarList.appendChild(listItem);
-}
-
-// Add class 'active' to section when near top of viewport
-function setActiveSections(section){
+// mark the section heading appearing now as active
+setActiveSections = (section) => {
     if(isElementAppearing(section.querySelector('h2'))){
         document.getElementById(section.id+'__link').classList.add('active');
         section.classList.add('active');
@@ -72,36 +40,34 @@ function setActiveSections(section){
     }
 }
 
-// Scroll to anchor ID using scrollTO event
-document.querySelectorAll('.menu__link').forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = event.target.id.split('__')[0];
-        window.scrollTo({
-            top: document.getElementById(targetId).offsetTop,
-            behavior: 'smooth'
+// scroll to the clicked section
+sectionScrolling = () => {
+    sectionLinks = document.querySelectorAll('.menu__link');
+    for(let sectionLink of sectionLinks){
+        sectionLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = event.target.id.split('__')[0];
+            window.scrollTo({
+                top: document.getElementById(targetId).offsetTop,
+                behavior: 'smooth'
+            });
         });
-    });
-});
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-function scrollToSection() {
-    for(let section of sections){
-        setActiveSections(section);
     }
 }
 
-// Set sections as active
-document.addEventListener('scroll', scrollToSection);
+/////////////////////////// Functions calling //////////////////////
+buildNavigationBar(sections);
+
+sectionScrolling();
+
+document.addEventListener('scroll', ()=>{
+    // call setActiveSections for each section
+    for(let section of sections){
+        setActiveSections(section);
+    }
+});
+/////////////////////////// End of Prgram ///////////////////////////
 
 const endingTime = performance.now();
+
 console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
